0658d69f8afe367b7666dd5696cfa1eafd5bdb65
Isabella Schindowski <isabella.schindowski@bosonit.com>
2023-08-22 12:51:52 +0200

    commit app hasta ejercicioCap4

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -2,7 +2,8 @@ package org.example
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.example.Libro.{Ejercicio2, Ejercicio_1, Ejercicio2Libro, Ejercicio_Cap3}
+import org.example.Borrador.borrador
+import org.example.Libro.{ejercicioCap2, ejercicioUDF, ejercicioCap3, ejercicioCap3b, ejercicioCap3c, ejercicioCap4}
 
 object App {
 
@@ -14,11 +15,17 @@ object App {
 
     spark.sparkContext.setLogLevel("ERROR")
 
-    //Ejercicio_1.ejercicio_1()
+    //ejercicioCap2.ejercicioCap2()
 
-    //Ejercicio2Libro.EjercicioLower()
+    //ejercicioUDF.ejercicioLower()
 
-    Ejercicio_Cap3.Ejercicio_Cap3()
+    //ejercicioCap3.ejercicioCap3()
+
+    //ejercicioCap3b.ejercicioCap3b()
+
+    //ejercicioCap3c.ejercicioCap3c()
+
+    ejercicioCap4.ejercicioCap4()
 
   }
 
